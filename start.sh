8024fba604fdf5b4808dcf110898512e74281f50
stmata <108939392+stmata@users.noreply.github.com>
2025-07-11 10:54:39 -0400

    new sh

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
-gunicorn app.main:app \
-  --chdir app \
-  --bind 0.0.0.0:8031 \
+gunicorn main:app \
+  --bind 0.0.0.0:$PORT \
   --workers 4 \
   --worker-class uvicorn.workers.UvicornWorker \
-  --reload \
   --timeout 360
 
